dc66f95703b7e4cb203888adc38d3d5dda9d3a10
Foteinopoulos <115899257+Foteinopoulos@users.noreply.github.com>
2022-11-04 20:04:10 +0200

    Add files via upload

--- social-network/src/main/java/com/dmst/president/SignUp.java
+++ social-network/src/main/java/com/dmst/president/SignUp.java
@@ -1,4 +1,5 @@
-import.java.util.Scanner;
+import java.util.Scanner;
+import java.util.Date;
 public class SignUp {
     String fname;
     String lname;
@@ -10,36 +11,36 @@ public class SignUp {
     String sdepartment;
     Scanner in = new Scanner(System.in);
     void ask_firstName() {
-        System.out.println("Please, give your first name")
+        System.out.println("Please, give your first name");
         fname = in.nextLine();
 
     }
     void ask_lastName() {
-         System.out.println("Please, give your last name")
+         System.out.println("Please, give your last name");
         lname = in.nextLine();
     }
     void  ask_userName() {
-        System.out.println("Please, give your username")
+        System.out.println("Please, give your username");
         uname = in.nextLine();
-    } 
+    }
     void  ask_password() {
-        System.out.println("Please, create your password")
+        System.out.println("Please, create your password");
         password = in.nextLine();
     }
     void ask_email() {
-        System.out.println("Please, give your email")
+        System.out.println("Please, give your email");
         mail = in.nextLine();
     }
     void ask_studentNumber() {
-        System.out.println("Please, give your student number")
+        System.out.println("Please, give your student number");
         snumber = in.nextLine();
     }
     void ask_date_of_Birth() {
-        System.out.println("Please, give your date of birth")
+        System.out.println("Please, give your date of birth");
         dbirth = in.nextLine();
     }
     void ask_studyDepartment() {
-        System.out.println("Please, give your study department")
+        System.out.println("Please, give your study department");
         sdepartment=in.nextLine();
     }
 }
