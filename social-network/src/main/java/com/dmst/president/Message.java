22bc3d65d4fa91471b304e66866beca50f1128ad
Christos Arapoglou <115351825+ChristosArapoglou@users.noreply.github.com>
2022-11-04 18:35:53 +0200

    Delete Message.java

--- social-network/src/main/java/com/dmst/president/Message.java
+++
@@ -1,9 +0,0 @@
-class Message {
-	
-	public final String WELCOME_TEXT = "Welcome to UniTalks, the first social media platform designed exclusively for AUEB students. Have fun!";
-	
-	public void welcome() {
-		System.out.println(WELCOME_TEXT);
-	}
-	
-}
