3988e100df21f07ede761ae8acb45f5b62a3b4bf
Foteinopoulos <115899257+Foteinopoulos@users.noreply.github.com>
2022-11-27 19:14:57 +0200

    Add files via upload

--- social-network/src/main/java/com/dmst/president/Login.java
+++ social-network/src/main/java/com/dmst/president/Login.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
-/*The class Login ask from user to enter his/her credentials,
-and accordingly to that,
+/**The class Login ask from user to enter his/her credentials,
+*and accordingly to that,
 he/she can connect in our website */
 public class Login {
-    User connection = new User();
+    private User connection = new User();
     private String username;
     private String password;
-    Scanner in = new Scanner(System.in);
-    /*Ask from user to enter his/her username*/
+    private Scanner in = new Scanner(System.in);
+    /**Ask from user to enter
+    *his/her username
+    */
     void askUserName() {
         System.out.println("Please, give your username");
         username = in .nextLine();
     }
-    /*Ask from user to enter his/her password*/
+    /**Ask from user
+    *to enter his/her password
+    */
     void askPassword() {
         System.out.println("Please, give your password");
         password = in .nextLine();
     }
-    public boolean stop = false;
-    /** The method connectornot verifies user's credentials 
+    protected boolean stop = false;
+    /** The method connectornot verifies user's credentials
     *via User class and prints messages
     about about the results of verification */
     void connectornot() {
@@ -37,7 +41,8 @@ public class Login {
                 askPassword();
             }
 
-        } while (stop = true);
+        }
+        while (stop = true);
     }
 }
 
