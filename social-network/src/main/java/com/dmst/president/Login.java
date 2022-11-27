5b040d650baf9bbe8574eb2e396ffbe5d5491dc9
Foteinopoulos <115899257+Foteinopoulos@users.noreply.github.com>
2022-11-27 19:04:24 +0200

    Add files via upload

--- social-network/src/main/java/com/dmst/president/Login.java
+++ social-network/src/main/java/com/dmst/president/Login.java
@@ -5,28 +5,27 @@ and accordingly to that,
 he/she can connect in our website */
 public class Login {
     User connection = new User();
-    String username;
-    String password;
+    private String username;
+    private String password;
     Scanner in = new Scanner(System.in);
     /*Ask from user to enter his/her username*/
     void askUserName() {
-        System.out.println("Please, give your username"); 
+        System.out.println("Please, give your username");
         username = in .nextLine();
     }
     /*Ask from user to enter his/her password*/
     void askPassword() {
-        System.out.println("Please, give your password"); 
+        System.out.println("Please, give your password");
         password = in .nextLine();
     }
-    boolean stop = false; 
-    /*The method connectornot verifies user's credentials
-    via User class and prints messages 
+    public boolean stop = false;
+    /** The method connectornot verifies user's credentials 
+    *via User class and prints messages
     about about the results of verification */
-
     void connectornot() {
         do {
             if (connection.verify(username, password)) {
-                System.out.println("Correct credentials, have a nice experience in our web");
+                System.out.println("Correct credentials, now you are online");
                 stop = true;
                 /*edo prepei na anoigei to parathyro ths efarmoghs */
             }   else {
@@ -39,7 +38,6 @@ public class Login {
             }
 
         } while (stop = true);
-
     }
 }
 
