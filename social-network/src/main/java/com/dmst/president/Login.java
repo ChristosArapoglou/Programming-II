ee78b59b33e3471e531f9b0d73414811b2dbf834
Foteinopoulos <115899257+Foteinopoulos@users.noreply.github.com>
2022-11-27 15:01:40 +0200

    Add files via upload

--- social-network/src/main/java/com/dmst/president/Login.java
+++ social-network/src/main/java/com/dmst/president/Login.java
@@ -1,33 +1,47 @@
-import java.util.Scanner;
+﻿import java.util.Scanner;
 
+ /*The class Login ask from user to enter his/her credentials and accordingly to that,
+    he/she can connect in our website */
 public class Login {
     User connection = new User();
     String username;
     String password;
     Scanner in = new Scanner(System.in);
     void ask_userName() {
-        System.out.println("Please, give your username");
-        username = in.nextLine();
+        System.out.println("Please, give your username"); /*Ask from user to enter his/her username*/
+        username = in .nextLine();
     }
     void ask_password() {
-        System.out.println("Please, give your password");
-        password=in.nextLine();
+        System.out.println("Please, give your password"); /*Ask from user to enter his/her password*/
+        password = in .nextLine();
     }
+    boolean stop = false;
     void connectornot() {
-        if (connection.verify(username, password)) {
-            System.out.println("Correct credentials, have a nice experience in our web");
-        }
-        else {
-            System.out.println("Sorry, your credentials are incorrect!");
-            System.out.println();
-            System.out.println("Please,try again!");
-            ask_userName();
-            ask_password();
-        }
+        do {
+            /*The method connectornot verifies user's credentials via User class */
+           /* and prints messages about the results of verification */
+
+
+            if (connection.verify(username, password)) {
+                System.out.println("Correct credentials, have a nice experience in our web");
+                
+                /*edo prepei na anoigei to parathyro ths efarmoghs */
+                
+            }   
+            else {
+                System.out.println("Sorry, your credentials are incorrect!");
+                System.out.println();
+                System.out.println("Please,try again!");
+
+                ask_userName();
+                ask_password();
+            }
+
+        } while (stop = true);
+
     }
+}
 /*  void logout(String username, String password) {
         username = null;
         password = null;
     }  */
-
-}
