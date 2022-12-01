b0f3ad1db8b576238d766863eb462051a4bbd935
Christos Arapoglou <115351825+ChristosArapoglou@users.noreply.github.com>
2022-12-01 21:26:34 +0200

    Update DatabaseTest according to Database recent changes.

--- social-network/src/main/java/com/dmst/president/DatabaseTest.java
+++ social-network/src/main/java/com/dmst/president/DatabaseTest.java
@@ -2,8 +2,9 @@ import java.sql.*;
 
 public class DatabaseTest {
 	public static void main(String[] args) {
+		//A dummy main created to test the functionality of Database.java
 		Connection dbcon = Database.initiateConnection();
-		Database.insertQuery(dbcon, "8210107", "giorgos", "papageorgiou", "g_pap", "123", "dmst", "2003-9-9");
-		Database.selectQuery(dbcon, "SN", "username");
+		Database.insertUser(dbcon, "8210107", "giorgos", "papageorgiou", "g_pap", "123", "dmst", "2003-9-9");
+		Database.selectUser(dbcon, "SN", "username");
 	}
 }
