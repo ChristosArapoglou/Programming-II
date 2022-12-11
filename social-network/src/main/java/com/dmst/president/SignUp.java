7dd9b9a28313d79ab68cb2b43480cc3c8617da49
AlexisMardas <t8210088@aueb.gr>
2022-12-11 18:14:11 +0200

    Update SignUp.java
    File merged had no .java extension and was not placed in the proper directory

--- social-network/src/main/java/com/dmst/president/SignUp.java
+++ social-network/src/main/java/com/dmst/president/SignUp.java
@@ -1,48 +1,68 @@
 package com.dmst.president;
 
 import java.util.Scanner;
-import java.util.Date;
-public class SignUp {
-    String fname;
-    String lname;
-    String uname;
-    String password;
-    String mail;
-    String snumber;
-    Date dbirth;
-    String sdepartment;
-    Scanner in = new Scanner(System.in);
-    void ask_firstName() {
-        System.out.println("Please, give your first name");
-        fname = in.nextLine();
+import java.util.GregorianCalendar;
 
+public class SignUp {
+    /*declaration of variables*/
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    String email;
+    String sn;
+    GregorianCalendar dob;
+    String stDept;
+    Scanner in = new Scanner(System.in);
+    /*welcoming the user to the platform*/
+    void welcomingUser() {
+    	System.out.println("Welcome to UniPost!");
+    	System.out.println("You will need to fill in your details below.");
     }
-    void ask_lastName() {
-         System.out.println("Please, give your last name");
-        lname = in.nextLine();
+    /*asks from the user to give his first name*/
+    void askFirstName() {
+        System.out.println("Please, give your first name");
+        firstName = in.nextLine();
     }
-    void  ask_userName() {
+    /*asks from the user to give his last name*/
+    void askLastName() {
+        System.out.println("Please, give your last name");
+        lastName = in.nextLine();
+    }
+    /*asks from the user to give his user name that he will use in the platform*/
+    void  askUserName() {
         System.out.println("Please, give your username");
-        uname = in.nextLine();
+        username = in.nextLine();
     }
-    void  ask_password() {
+    /*asks from the user to create password for his user name*/ 
+    void  askPassword() {
         System.out.println("Please, create your password");
         password = in.nextLine();
     }
-    void ask_email() {
+    /*asks from the user to give his email*/  
+    void askEmail() {
         System.out.println("Please, give your email");
-        mail = in.nextLine();
+        email = in.nextLine();
     }
-    void ask_studentNumber() {
+    /*asks from the user to give his student number*/   
+    void askStudentNumber() {
         System.out.println("Please, give your student number");
-        snumber = in.nextLine();
+        sn = in.nextLine();
     }
-    void ask_date_of_Birth() {
-        System.out.println("Please, give your date of birth");
-        //dbirth = in.nextLine();
-    }
-    void ask_studyDepartment() {
-        System.out.println("Please, give your study department");
-        sdepartment=in.nextLine();
+    /*asks from the user to give his date of birth*/
+     void askDateOfBirth() {
+        System.out.print("Please, enter your date of birth: ");
+        System.out.println("Year: ");
+        int year = in.nextInt();
+        System.out.println("Month: ");
+        int month = in.nextInt();
+        System.out.println("Day: ");
+        int day = in.nextInt();    
+        dob = new GregorianCalendar(year, month, day);
+     }
+     /*asks from the user to give his study department*/
+     void askStudyDepartment() {
+        System.out.println("Please, enter your study department.");
+        stDept = in.nextLine();
     }
 }
