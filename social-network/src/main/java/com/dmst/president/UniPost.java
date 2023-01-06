e8aa6d1434d39b5e28b63f16f7c6786d733b7437
Christos Gousis <t8210029@aueb.gr>
2023-01-06 02:09:42 +0200

    Added delay and clearConsole on displayLoginSignupPage

--- social-network/src/main/java/com/dmst/president/UniPost.java
+++ social-network/src/main/java/com/dmst/president/UniPost.java
@@ -56,6 +56,8 @@ public class UniPost {
                 * then checked if it meets the criteria */
             if (!(ans.toLowerCase().equals("s") || ans.toLowerCase().equals("l"))) {
                     System.out.println("Wrong answer");
+                    delay(500);
+                    clearConsole();
                     flag = true;
             }
         } while (flag);
